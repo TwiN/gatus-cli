8de8d0225a3028eba97de974b111e2725f740ce1
TwiN <chris@twin.sh>
2025-05-10 18:57:12 -0400

    fix: Rename --id flag to --status-page-id

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This is a client for Gatus.io
 ## Usage
 ### Retrieving a status page by ID
 ```console
-gatus-client status-page get --id 12345
+gatus-client status-page get --status-page-id 12345
 ```
--- cmd/status_page.go
+++ cmd/status_page.go
@@ -21,11 +21,11 @@ var statusPageCmd = &cobra.Command{
 var statusPageGetCmd = &cobra.Command{
 	Use:     "get",
 	Short:   "Retrieve a status page",
-	Example: `  gatus-client status-page get --id 12345`,
+	Example: `  gatus-client status-page get --status-page-id 12345`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		statusPageID, _ := cmd.Flags().GetInt("id")
+		statusPageID, _ := cmd.Flags().GetInt("status-page-id")
 		if statusPageID == 0 {
-			return errors.New("missing flag --id")
+			return errors.New("missing flag --status-page-id")
 		}
 		apiKey := config.GetAPIKey()
 		request, err := http.NewRequest("GET", "https://gatus.io/api/v1/external/status-pages/"+strconv.Itoa(statusPageID), nil)
@@ -55,6 +55,6 @@ func init() {
 	rootCmd.SilenceUsage = true
 	rootCmd.SilenceErrors = true
 	rootCmd.AddCommand(statusPageCmd)
-	statusPageGetCmd.Flags().IntP("id", "i", 0, "ID of the status page")
+	statusPageGetCmd.Flags().IntP("status-page-id", "s", 0, "ID of the status page")
 	statusPageCmd.AddCommand(statusPageGetCmd)
 }
